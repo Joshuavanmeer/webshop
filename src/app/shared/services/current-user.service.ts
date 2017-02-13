94dda8dc5bbb65bc1417281edf25fcc69f9fe4c6
Joshuavm <joshuavanmeer@gmail.com>
2017-02-13 11:10:06 +0100

    added class files

---
+++ src/app/shared/services/current-user.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class CurrentUserService {
+
+
+    private wishList: WishList;
+    private shoppingCart: ShoppingCart;
+
+
+
+
+
+    constructor() { }
+
+
+
+
+}
