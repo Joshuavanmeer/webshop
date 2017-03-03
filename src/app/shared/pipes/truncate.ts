5cde33532986f804a67248656582f69161fa359d
Joshuavm <joshuavanmeer@gmail.com>
2017-03-03 18:07:23 +0100

    added minor styling

--- src/app/shared/pipes/truncate.ts
+++ src/app/shared/pipes/truncate.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TruncatePipe implements PipeTransform {
 
     transform(value: string, args: string[]) : string {
-        let limit = args.length > 0 ? parseInt(args[0], 100) : 100;
+        let limit = args.length > 0 ? parseInt(args[0], 90) : 90;
         let trail = args.length > 1 ? args[1] : '...';
 
         return value.length > limit ? value.substring(0, limit) + trail : value;
